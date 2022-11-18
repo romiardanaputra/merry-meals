6ec1731e2b4425317694e556fc8270c238a84509
Kadek Romi Ardana Putra <romiardana21@gmail.com>
2022-11-18 23:21:26 +0800

    update register and login with new design

--- resources/views/components/login.blade.php
+++ resources/views/components/login.blade.php
@@ -11,9 +11,10 @@
 </head>
 
 <body class="font-poppins">
-@include('components.navbar')
+    @include('components.navbar')
     <div class="bg-login h-fit w-full bg-[#FFFCF0] py-[138px] px-[147px]">
-        <div style="box-shadow: 0px 8px 50px rgba(174, 168, 135, 0.5);" class="bg-form-login h-[789px] w-full bg-[#FFFCF0] px-[86px] py-[169px] flex flex-row justify-center items-center space-x-[108px]">
+        <div style="box-shadow: 0px 8px 50px rgba(174, 168, 135, 0.5);"
+            class="bg-form-login h-[789px] w-full bg-[#FFFCF0] px-[86px] py-[169px] flex flex-row justify-center items-center space-x-[108px]">
 
             <div class="form-login-text text-center text-[#282222] w-[70%]">
                 <h1 class="font-semibold text-[40px]">Welcome back!</h1>
@@ -21,29 +22,62 @@
             </div> <!-- form-login-text -->
 
             <div class="form-login-fields h-fit w-full">
-                <form action="/login">        
+                <form action="{{ route('login.authenticate') }}">
+                    @csrf
+                    {{--authentication allert success --}}
+                    @if (session()->has('success'))
+                    <div class="alert alert-success shadow-lg">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6"
+                                fill="none" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            <span>{{ session('success') }}</span>
+                        </div>
+                    </div>
+                    @endif
+                    {{--authentication allert failed --}}
+                    @if (session()->has('login_error'))
+                    <div class="alert alert-error shadow-lg">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6"
+                                fill="none" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            <span>{{ session('login_error') }}</span>
+                        </div>
+                    </div>
+                    @endif
+
                     <div class="flex flex-col space-y-[24px] mb-[60px]">
                         <div class="text-gray-500 w-full">
                             <label for="email">Email*</label>
-                            <input type="email" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3">
+                            <input type="email"
+                                class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3 @error('email')@enderror"
+                                name="email">
 
-                            @error('first_name')
+                            @error('email')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
-                        
+
                         <div class="text-gray-500 w-full">
                             <label for="password">Password*</label>
-                            <input type="password" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3">
+                            <input type="password"
+                                class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3 @error('email')@enderror"
+                                name="password">
 
-                            @error('first_name')
+                            @error('password')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
                     </div> <!-- form-register-flex -->
 
                     <div class="flex justify-center">
-                        <button type="submit" class="mt-[30px] h-[35px] w-[145.08px] border-2 border-[#A07C00] bg-[#FFFCF0] font-medium text-[16px] text-[#A07C00] hover:scale-105 duration-500 mb-[29px]">Login</button>
+                        <button type="submit"
+                            class="mt-[30px] h-[35px] w-[145.08px] border-2 border-[#A07C00] bg-[#FFFCF0] font-medium text-[16px] text-[#A07C00] hover:scale-105 duration-500 mb-[29px]">Login</button>
                     </div> <!-- button-flex -->
 
                     <div class="link-register flex flex-row font-medium text-[16px] space-x-[5px] justify-center">
@@ -55,69 +89,7 @@
 
         </div> <!-- bg-form-login -->
     </div> <!-- bg-login -->
-@include('components.footer')
-    <!-- <section class="font-sans h-screen flex items-center justify-center">
-        <div class="form-section container mx-auto">
-            @if (session()->has('success'))
-            <div class="alert alert-success shadow-lg">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none"
-                        viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <span>{{ session('success') }}</span>
-                </div>
-            </div>
-            @endif
-
-            @if (session()->has('login_error'))
-            <div class="alert alert-error shadow-lg">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none"
-                        viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <span>{{ session('login_error') }}</span>
-                </div>
-            </div>
-            @endif
-
-            <div class="max-w-full h-full">
-                <form action="/login" method="POST" class="shadow-lg w-1/2 rounded-lg m-auto">
-                    @csrf
-                    <h1 class="text-5xl font-semibold uppercase leading-relaxed text-center pt-[2rem]">{{ $name_content
-                        }}</h1>
-                    <div class="form-detail px-[10rem] py-[5rem] flex justify-center items-center flex-col">
-                        <div class="mb-6 w-full block  ">
-                            <input class="input input-bordered input-secondary w-full block px-6 py-6 @error('email_user') @enderror"
-                                id="email_user" name="email_user" type="text" required placeholder="Email Address">
-                            @error('email_user')
-                            <div class="text-red-600 block">
-                                <p class="text-left">{{ $message }}</p>
-                            </div>
-                            @enderror
-                        </div>
-                        <div class="mb-6 w-full block  ">
-                            <input class="input input-bordered input-secondary w-full block px-6 py-6 @error('password') @enderror"
-                                id="password" name="password" type="password" required placeholder="Password">
-                            @error('password')
-                            <div class="text-red-600 block">
-                                <p class="text-left">{{ $message }}</p>
-                            </div>
-                            @enderror
-                        </div>
-                        <button
-                            class="border border-orange-300 p-3 rounded-md text-orange-300 hover:text-white hover:bg-orange-300 w-full hover:transition duration-500 ease-in-out font-semibold"
-                            type="submit">SIGN IN</button>
-                        <span class="mt-[4rem]">Don't Have An Account ? <a class="text-blue-500" href="/register">Sign
-                                Up Here !</a></span>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </section> -->
+    @include('components.footer')
 </body>
 
 </html>
--- resources/views/components/register.blade.php
+++ resources/views/components/register.blade.php
@@ -21,22 +21,23 @@
             </div> <!-- form-register-text -->
 
             <div class="form-register-fields w-full">
-                <form action="/register" class="flex flex-col space-y-[24px]">        
+                <form action="{{ route('registration.store') }}" class="flex flex-col space-y-[24px]">
+                    @csrf        
                     <div class="flex flex-row space-x-[45px]">
                         <div class="text-gray-500 w-1/2">
                             <label for="full-name">Full Name*</label>
-                            <input type="text" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3">
+                            <input type="text" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3 @error('full_name')@enderror" name="full_name">
 
-                            @error('first_name')
+                            @error('full_name')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
                         
                         <div class="text-gray-500 w-1/2">
                             <label for="username">Username*</label>
-                            <input type="text" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3">
+                            <input type="text" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3 @error('usernmae')@enderror" name="username">
 
-                            @error('first_name')
+                            @error('username')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
@@ -45,18 +46,18 @@
                     <div class="flex flex-row space-x-[45px]">
                         <div class="text-gray-500 w-1/2">
                             <label for="email">Email*</label>
-                            <input type="email" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3">
+                            <input type="email" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3 @error('email')@enderror" name="email">
 
-                            @error('first_name')
+                            @error('email')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
                         
                         <div class="text-gray-500 w-1/2">
                             <label for="phone-number">Phone Number*</label>
-                            <input type="text" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3">
+                            <input type="text" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3 @error('phone_number')@enderror" name="phone_number">
 
-                            @error('first_name')
+                            @error('phone_number')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
@@ -65,23 +66,23 @@
                     <div class="flex flex-row space-x-[45px]">
                         <div class="text-gray-500 w-1/2">
                             <label for="age">Age (yr)*</label>
-                            <input type="number" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3">
+                            <input type="number" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3 @error('age')@enderror" name="age">
 
-                            @error('first_name')
+                            @error('age')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
                         
                         <div class="text-gray-500 w-1/2">
                             <label for="role">Register as*</label>                            
-                            <select class="w-full bg-[#FFFCF0] border border-gray-400 text-gray-500 py-2 px-3 pr-8 leading-tight focus:outline-none focus:bg-[#FFFCF0] focus:border-gray-500" id="role">
+                            <select class="w-full bg-[#FFFCF0] border border-gray-400 text-gray-500 py-2 px-3 pr-8 leading-tight focus:outline-none focus:bg-[#FFFCF0] focus:border-gray-500 @error('role')@enderror" id="role" name="role">
                                 <option class="block">Please select your role</option>
                                 <option>Member</option>
                                 <option>Volunteer</option>
                                 <option>Caregiver</option>
                             </select>                           
 
-                            @error('first_name')
+                            @error('role')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
@@ -90,9 +91,9 @@
                     <div class="flex flex-row space-x-[45px]">
                         <div class="text-gray-500 w-full">
                             <label for="address">Address*</label>
-                            <input type="number" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3">
+                            <input type="text" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3 @error('address')@enderror" name="address">
 
-                            @error('first_name')
+                            @error('address')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
@@ -101,18 +102,18 @@
                     <div class="flex flex-row space-x-[45px]">
                         <div class="text-gray-500 w-1/2">
                             <label for="password">Password*</label>
-                            <input type="password" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3">
+                            <input type="password" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3 @error('password')@enderror" name="password">
 
-                            @error('first_name')
+                            @error('password')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
                         
                         <div class="text-gray-500 w-1/2">
                             <label for="confirm-password">Confirm Password*</label>
-                            <input type="password" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3">
+                            <input type="password" class="bg-[#FFFCF0] border border-gray-400 w-full py-2 px-3 @error('confirm_password')@enderror" name='confirm_password'>
 
-                            @error('first_name')
+                            @error('confirm_password')
                             <p class="text-red-500">{{ $message }}</p>
                             @enderror
                         </div> <!-- text -->
@@ -127,103 +128,6 @@
         </div> <!-- bg-form-register -->
     </div> <!-- bg-register -->
     @include('components.footer')
-
-    {{-- <section class="font-sans h-screen flex items-center justify-center">
-        <div class="form-section container mx-auto">
-            <div class="max-w-full h-full">
-                <form action="/register" method="POST" class="shadow-lg w-1/2 rounded-lg m-auto">
-                    <h1 class="text-5xl font-semibold uppercase leading-relaxed text-center pt-[2rem]">{{ $name_content
-                        }}</h1>
-                    @csrf
-                    <div class="form-detail px-[10rem] py-[5rem] flex justify-center items-center flex-col">
-                        <div class="mb-6 w-full block  ">
-                            <input
-                                class="input input-bordered input-secondary w-full block px-6 py-6 @error('first_name') @enderror"
-                                id="first_name" name="first_name" type="text" required placeholder="Firstname">
-                            @error('first_name')
-                            <div class="text-red-600 block">
-                                <p class="text-left">{{ $message }}</p>
-                            </div>
-                            @enderror
-                        </div>
-                        <div class="mb-6 w-full block">
-                            <input
-                                class="input input-bordered input-secondary w-full block px-6 py-6 @error('last_name') @enderror"
-                                id="last_name" name="last_name" type="text" required placeholder="Lastname">
-                            @error('last_name')
-                            <div class="text-red-600 block">
-                                <p class="text-left">{{ $message }}</p>
-                            </div>
-                            @enderror
-                        </div>
-                        <div class="mb-6 w-full block  ">
-                            <input
-                                class="input input-bordered input-secondary w-full block px-6 py-6 @error('username') @enderror"
-                                id="username" name="username" type="text" required placeholder="Username">
-                            @error('username')
-                            <div class="text-red-600 block">
-                                <p class="text-left">{{ $message }}</p>
-                            </div>
-                            @enderror
-                        </div>
-                        <div class="mb-6 w-full block  ">
-                            <input
-                                class="input input-bordered input-secondary w-full block px-6 py-6 @error('email_user') @enderror"
-                                id="email_user" name="email_user" type="text" required placeholder="Email Address">
-                            @error('email_user')
-                            <div class="text-red-600 block">
-                                <p class="text-left">{{ $message }}</p>
-                            </div>
-                            @enderror
-                        </div>
-                        <div class="mb-6 w-full block  ">
-                            <input
-                                class="input input-bordered input-secondary w-full block px-6 py-6 @error('password') @enderror"
-                                id="password" name="password" type="password" required placeholder="Password">
-                            @error('password')
-                            <div class="text-red-600 block">
-                                <p class="text-left">{{ $message }}</p>
-                            </div>
-                            @enderror
-                        </div>
-                        <div class="mb-6 w-full block  ">
-                            <input
-                                class="input input-bordered input-secondary w-full block px-6 py-6 @error('phone_number') @enderror"
-                                id="phone_number" name="phone_number" type="text" required placeholder="phone_number">
-                            @error('phone_number')
-                            <div class="text-red-600 block">
-                                <p class="text-left">{{ $message }}</p>
-                            </div>
-                            @enderror
-                        </div>
-                        <div class="mb-6 w-full block  ">
-                            <select class="select select-secondary w-full" name="role" id="role">
-                                <option>member</option>
-                                <option>caregiver</option>
-                                <option>volunteer</option>
-                            </select>
-                        </div>
-                        <div class="mb-6 w-full block  ">
-                            <textarea
-                                class="input input-bordered input-secondary w-full block px-6 py-6 @error('address') @enderror"
-                                id="address" name="address" type="text" required
-                                placeholder="address"></textarea>
-                            @error('address')
-                            <div class="text-red-600 block">
-                                <p class="text-left">{{ $message }}</p>
-                            </div>
-                            @enderror
-                        </div>
-                        <button
-                            class="border border-orange-300 p-3 rounded-md text-orange-300 hover:text-white hover:bg-orange-300 w-full hover:transition duration-500 ease-in-out font-semibold"
-                            type="submit">SIGN UP</button>
-                        <span class="mt-[4rem]">Already Have An Account ? <a class="text-blue-500" href="/login">Sign In
-                                Here !</a></span>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </section> --}}
 </body>
 
 </html>
